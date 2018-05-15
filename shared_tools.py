58a6c34ac08610d771e063d3a3b860b895b2931e
AJ <aj.wilson@scouting.org>
2018-05-15 15:54:51 -0500

    WIP - Linear Regression & Basic Project Tools Outline

---
+++ shared_tools.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+This is a collections of functions common to the other, individual pieces of this project
+"""
+
+__author__ = "AJ Wilson"
+__copyright__ = " "
+__license__ = " "
+__version__ = "0.0.0.0"
+__maintainer__ = "AJ Wilson"
+__email__ = "aj.wilson08[at]gmail.com"
+__status__ = "WIP"
+
+
+def function1():
+    return None
